1f9132c47ea4e13a7d242c686c95b448c1077b0a
Silalieva Begimay <begimayomurovna@gmail.com>
2024-01-30 14:34:38 +0600

    Добавление базовой структуры в компонент App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Chat from "./Components/Chat/Chat";
+
+function App() {
+    const author = "User";
+    return (
+        <>
+            <div className="row">
+                <Chat author={author}/>
+            </div>
+        </>
+    );
+}
+
+export default App;
